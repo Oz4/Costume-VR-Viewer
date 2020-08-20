7a277d3a42d51f8f650ef542ee8906157e464f72
Aws Aldarwish <aowss18@gmail.com>
2020-08-20 11:19:25 +0300

    Full_Project_Upload

--- script.js
+++ script.js
@@ -5,25 +5,40 @@ import { PLYLoader } from './three.js-master/examples/jsm/loaders/PLYLoader.js';
 import { Water } from './three.js-master/examples/jsm/objects/Water.js';
 import { GUI } from './three.js-master/examples/jsm/libs/dat.gui.module.js'
 import { VRButton } from './three.js-master/examples/jsm/webxr/VRButton.js';
-let  camera, renderer, controls, water, ground;
+import { EffectComposer } from './three.js-master/examples/jsm/postprocessing/EffectComposer.js';
+import { RenderPass } from './three.js-master/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from './three.js-master/examples/jsm/postprocessing/ShaderPass.js';
+let camera, renderer, controls, water, ground, button;
+let leftCamera, rightCamera;
+let composer, effect;
+
 let mainScene = new THREE.Scene();
+
 
 let params = {
     PointCloud: false,
     scenes: "Tower Scene",
     VR: false,
     eyeSeperation: 0.083,
-    autorotate:true,
-    crosseffect:false
+    distortionstrength: 2,
+    autorotate: false,
+    crosseffect: false,
+    cylindricalRatio: 2,
+    fov: 45
 };
+
 
 let gui = new GUI();
 gui.add(params, 'PointCloud').name('Point Cloud');
 gui.add(params, 'scenes', ['Tower Scene', 'Mosque Scene']);
 gui.add(params, 'VR').name('VR');
 gui.add(params, 'eyeSeperation').name('Eye Seperation');
+gui.add(params, 'distortionstrength').name('Fish Eye Strength');
 gui.add(params, 'autorotate').name('Rotate Camera');
 gui.add(params, 'crosseffect').name('Cross-Effect');
+gui.add(params, 'cylindricalRatio').name('cylindricalRatio');
+gui.add(params, 'fov').name('fov');
+
 
 
 let manager = new THREE.LoadingManager();
@@ -45,18 +60,24 @@ let Setup = function () {
     renderer.xr.enabled = true;
     renderer.setSize(window.innerWidth, window.innerHeight);
     let container = document.getElementById('container');
+    button = document.getElementById('button');
     container.appendChild(renderer.domElement);
-    container.appendChild(VRButton.createButton(renderer));
+    button.appendChild(VRButton.createButton(renderer));
+    console.log(container.getElementsByTagName('a'));
+    composer = new EffectComposer(renderer);
+
+
+    effect = new ShaderPass(getDistortionShaderDefinition());
 
 }
 let SceneSetup = function (currentScene) {
-    camera.position.z = 20;
+    camera.position.z = 10;
     camera.position.x = 5;
     camera.position.y = 5
     currentScene.background = new THREE.Color(0xdddddd);
 
     controls = new OrbitControls(camera, renderer.domElement);
-    controls.minDistance = 5;
+    controls.minDistance = 0.1;
     controls.maxDistance = 60;
     controls.maxPolarAngle = Math.PI / 2 - 0.1;
     controls.autoRotateSpeed = 2;
@@ -94,7 +115,6 @@ let AddWater = function (currentScene) {
     water.rotation.x = -1 * Math.PI / 2;
     water.visible = false;
     currentScene.add(water);
-
 
 }
 let AddGround = function (currentScene) {
@@ -200,21 +220,37 @@ let isMobileDevice = function () {
         return true;
 }
 let animate = function () {
+
+
     renderer.setAnimationLoop(function () {
         if (params['autorotate']) {
             controls.autoRotate = true;
             controls.update();
         }
-        else{
-            controls.autoRotate = false ;
+        else {
+            controls.autoRotate = false;
         }
-        
+
 
         if (params['VR']) {
-
+            if (!document.fullscreenElement && document.fullscreenEnabled) {
+                document.documentElement.requestFullscreen();
+            }
+            document.getElementById("vr_image").style.visibility = "visible";
+            if (button != null) button.remove();
             renderVR();
         }
         else {
+            if (document.fullscreenElement) {
+                document.exitFullscreen();
+            }
+            document.getElementById("vr_image").style.visibility = "hidden";
+            if (document.getElementById('button') == null) {
+                button = document.createElement("div");
+                button.id = 'button';
+                container.appendChild(button);
+                button.appendChild(VRButton.createButton(renderer));
+            }
 
             render();
         }
@@ -232,7 +268,7 @@ let animate = function () {
             }
             else {
                 mainScene.getObjectByName("towerPC").visible = false;
-                mainScene.getObjectByName("towerOBJ").visible = true;
+                mainScene.getObjectByName("towerOBJ").visible = true;//-----true
             }
         }
         if (params['scenes'] == 'Mosque Scene') {
@@ -256,26 +292,25 @@ let animate = function () {
 let renderVR = function () {
 
 
-    let rightEye = new THREE.Matrix4();
-    let leftEye = new THREE.Matrix4();
-    console.log(camera.near / 12);
-    let aspect = camera.aspect * 0.5;
+    const rightEye = new THREE.Matrix4();
+    const leftEye = new THREE.Matrix4();
+    const aspect = camera.aspect * 0.5;
     let eyeSeperation = params['eyeSeperation'];
-    let leftCamera = new THREE.PerspectiveCamera();
+    leftCamera = new THREE.PerspectiveCamera();
     leftCamera.matrixAutoUpdate = false;
 
-    let rightCamera = new THREE.PerspectiveCamera();
+    rightCamera = new THREE.PerspectiveCamera();
     rightCamera.matrixAutoUpdate = false;
-    if (params['crosseffect'])  eyeSeperation *= -1;
-    
+    if (params['crosseffect']) eyeSeperation *= -1;
+
     camera.updateMatrixWorld();
 
-    let projectionMatrix = camera.projectionMatrix.clone();
-    let  halfWidth  = (camera.near * Math.tan(Math.PI / 180 * camera.fov * 0.5));//  camera near * cemera apreture in radian degree
+    const projectionMatrix = camera.projectionMatrix.clone();
+    const halfWidth = (camera.near * Math.tan(Math.PI / 180 * camera.fov * 0.5));//  camera near * cemera apreture in radian degree
     let left, right;
 
     // adding the eyeseperation to matrix4 for transformation of the view(world matrix)
-    leftEye.elements[12] = - eyeSeperation / 2; 
+    leftEye.elements[12] = - eyeSeperation / 2;
     rightEye.elements[12] = eyeSeperation / 2;
 
     //using the halfwidth we can estimate how much seperation depending on the focal lenght
@@ -290,33 +325,50 @@ let renderVR = function () {
 
     leftCamera.projectionMatrix.copy(projectionMatrix);
 
-    //================== right eye
+/* 
+    projection Matrix = 
+    [2n/r-l     0      r+l/r-l     0     ]          
+    [  0      2n/t-b   t+b/t-b     0     ]
+    [  0        0     -f+n/f-n  -2fn/f-n ]
+    [  0        0        -1        0     ]
 
+*/  
+
+    //================== right eye
     left = - halfWidth * aspect - (eyeSeperation / 2) * camera.near / camera.focus;
     right = halfWidth * aspect - (eyeSeperation / 2) * camera.near / camera.focus;
-    // update camera projection matrix 
-    projectionMatrix.elements[0] = 2 * camera.near / (right - left);
-    projectionMatrix.elements[8] = (right + left) / (right - left);
+    // update camera projection matrix , indexing is colomn based in three js
+    // update r and l in projection matrix
+    projectionMatrix.elements[0] = 2 * camera.near / (right - left); //2n/r-l
+    projectionMatrix.elements[8] = (right + left) / (right - left);// r+l / r-l
 
     rightCamera.projectionMatrix.copy(projectionMatrix);
 
     //copy world matrix to both cameras
     leftCamera.matrixWorld.copy(camera.matrixWorld).multiply(leftEye);
     rightCamera.matrixWorld.copy(camera.matrixWorld).multiply(rightEye);
+    //-----------------------------------------
 
-    //rendering the 2 views
+
+    //rendering the 2 views by splitting with scissor views
     renderer.setScissorTest(true);
 
+    //redner left view
     renderer.setScissor(0, 0, window.innerWidth / 2, window.innerHeight);
     renderer.setViewport(0, 0, window.innerWidth / 2, window.innerHeight);
-    renderer.render(mainScene,leftCamera);
+    composer.addPass(new RenderPass(mainScene, leftCamera));
+    composer.addPass(effect);
+    fishEyeRender(leftCamera);
 
+    //render right view
     renderer.setScissor(window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
     renderer.setViewport(window.innerWidth / 2, 0, window.innerWidth / 2, window.innerHeight);
-    renderer.render(mainScene, rightCamera);
+    composer.addPass(new RenderPass(mainScene, rightCamera));
+    composer.addPass(effect);
+    fishEyeRender(rightCamera);
 
     renderer.setScissorTest(false);
-
+    composer.clearPasses();//costume function
     //-----------------------------------------
 
 }
@@ -325,30 +377,107 @@ let render = function () {
     renderer.setScissorTest(false);
     renderer.render(mainScene, camera);
 }
+let loadSceneObjects = function(){
+    if (isMobileDevice()) {
+
+        AddPointCloudModel(mainScene, 'Mosque_PC.ply', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.5, 'mosquepc');
+        AddOBJModel(mainScene, 'Mosque_LQ.obj', 'mosque_Texture.png', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 'mosqueOBJ');
+    
+        AddOBJModel(mainScene, 'Kiz_Kulasi_Mobile.obj', 'lq_kiz_texture.png', 0, 3.6, -3, 1, 0, Math.PI + 0.3, -0.035, 'towerOBJ');
+        AddPointCloudModel(mainScene, 'Kiz_Kulasi_PC_Mobile.ply', 4.1, 5.9, -12, 1, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.004, 'towerPC');
+    }
+    else {
+        AddPointCloudModel(mainScene, 'Mosque_PC.ply', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.2, 'mosquepc');
+        AddOBJModel(mainScene, 'Mosque_LQ.obj', 'mosque_Texture.png', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 'mosqueOBJ');
+    
+        AddOBJModel(mainScene, 'Kiz_kulasi_HQ.obj', 'lq_kiz_texture.png', 0, 3.6, -3, 1, 0, Math.PI + 0.3, -0.035, 'towerOBJ');
+        AddPointCloudModel(mainScene, 'Kiz_Kulasi_PC.ply', 4.1, 5.9, -12, 1, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.004, 'towerPC');
+    }
+}
+
+
 window.addEventListener('resize', onWindowResize, false);
-
-
 Setup();
 SceneSetup(mainScene);
 AddGround(mainScene);
 AddWater(mainScene);
-if (isMobileDevice()) {
-
-    AddPointCloudModel(mainScene, 'mosquepointcloud.ply', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.5, 'mosquepc');
-    AddOBJModel(mainScene, 'lowqualitymosque2.obj', 'texture2.png', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 'mosqueOBJ');
-
-    AddOBJModel(mainScene, 'tower_mobile2.obj', 'lq_kiz_texture.png', 0, 3.6, -3, 1, 0, Math.PI + 0.3, -0.035, 'towerOBJ');
-    AddPointCloudModel(mainScene, 'tower_Dense_mobile.ply', 4.1, 5.9, -12, 1, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.004, 'towerPC');
-}
-else {
-    AddPointCloudModel(mainScene, 'mosquepointcloud.ply', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.2, 'mosquepc');
-    AddOBJModel(mainScene, 'lowqualitymosque2.obj', 'texture2.png', -50, 22, -5, 10, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 'mosqueOBJ');
-
-    AddOBJModel(mainScene, 'Desktop6.obj', 'lq_kiz_texture.png', 0, 3.6, -3, 1, 0, Math.PI + 0.3, -0.035, 'towerOBJ');
-    AddPointCloudModel(mainScene, 'tower_Dense_reconstruction.0.ply', 4.1, 5.9, -12, 1, 180 / 180 * Math.PI, -20 / 180 * Math.PI, -0.035, 0.004, 'towerPC');
-}
+loadSceneObjects();
 AddCubeMap(mainScene);
+
+
 manager.onLoad = function () {
     console.log('Loading complete!');
     animate();
 };
+//Costume fish eye shader
+//the composereffect was edited to clear passes each iteration to release the ram.
+function getDistortionShaderDefinition() {
+    return {
+
+        uniforms: {
+            "tDiffuse": { type: "t", value: null },
+            "strength": { type: "f", value: 0 },
+            "height": { type: "f", value: 1 },
+            "aspectRatio": { type: "f", value: 1 },
+            "cylindricalRatio": { type: "f", value: 1 }
+        },
+
+        vertexShader: [
+            "uniform float strength;",
+            "uniform float height;",
+            "uniform float aspectRatio;",
+            "uniform float cylindricalRatio;",
+
+            "varying vec3 vUV;",
+            "varying vec2 vUVDot;",
+
+            "void main() {",
+            "gl_Position = projectionMatrix * (modelViewMatrix * vec4(position, 1.0));",
+
+            "float scaledHeight = strength * height;",
+            "float cylAspectRatio = aspectRatio * cylindricalRatio;",
+            "float aspectDiagSq = aspectRatio * aspectRatio + 1.0;",
+            "float diagSq = scaledHeight * scaledHeight * aspectDiagSq;",
+            "vec2 signedUV = (2.0 * uv + vec2(-1.0, -1.0));",
+
+            "float z = 0.5 * sqrt(diagSq + 1.0) + 0.5;",
+            "float ny = (z - 1.0) / (cylAspectRatio * cylAspectRatio + 1.0);",
+
+            "vUVDot = sqrt(ny) * vec2(cylAspectRatio, 1.0) * signedUV;",
+            "vUV = vec3(0.5, 0.5, 1.0) * z + vec3(-0.5, -0.5, 0.0);",
+            "vUV.xy += uv;",
+            "}"
+        ].join("\n"),
+
+        fragmentShader: [
+            "uniform sampler2D tDiffuse;",
+            "varying vec3 vUV;",
+            "varying vec2 vUVDot;",
+
+            "void main() {",
+            "vec3 uv = dot(vUVDot, vUVDot) * vec3(-0.5, -0.5, -1.0) + vUV;",
+            "gl_FragColor = texture2DProj(tDiffuse, uv);",
+            "}"
+        ].join("\n")
+
+    };
+}
+let fishEyeRender = function (cache_camera) {
+
+    let horizontalFOV = params['fov'];
+    let strength = params['distortionstrength'];
+    let cylindricalRatio = params['cylindricalRatio'];
+    let height = Math.tan(THREE.Math.degToRad(horizontalFOV) / 2) / cache_camera.aspect;
+
+    cache_camera.fov = Math.atan(height) * 2 * 180 / Math.PI;
+    cache_camera.updateProjectionMatrix();
+
+    effect.uniforms["strength"].value = strength;
+    effect.uniforms["height"].value = height;
+    effect.uniforms["aspectRatio"].value = cache_camera.aspect;
+    effect.uniforms["cylindricalRatio"].value = cylindricalRatio;
+
+    composer.render(mainScene, cache_camera);
+
+}
+
